5ef3352b6f61dfbbeb5e901739b0c5202ee4a065
tovoperry <236172690+Tovo-voo@users.noreply.github.com>
2026-04-09 11:13:48 +0800

    collectstatic verbosity

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --verbosity 2
 python manage.py migrate
 python manage.py loaddata fixtures/initial_data.json
