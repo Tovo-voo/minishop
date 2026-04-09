db294d2fae28e90e93e6e8044b33bde4c491aff5
tovoperry <236172690+Tovo-voo@users.noreply.github.com>
2026-04-09 12:06:48 +0800

    嘗試直接複製static

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input --clear --verbosity 2
+mkdir -p staticfiles
+cp -r static/* staticfiles/
 python manage.py migrate
 python manage.py loaddata fixtures/initial_data.json
