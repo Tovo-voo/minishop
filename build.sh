5ecdacb22a3805440f7c51517073ea48a16fa807
tovoperry <236172690+Tovo-voo@users.noreply.github.com>
2026-04-09 11:52:50 +0800

    clear強制清除舊檔，重新收集

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input --verbosity 2
+python manage.py collectstatic --no-input --clear --verbosity 2
 python manage.py migrate
 python manage.py loaddata fixtures/initial_data.json
